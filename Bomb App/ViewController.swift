306e4d6c45ee58f1b9b053b1e28f4184b768898a
Patrick Morton <patmort02@gmail.com>
2015-09-12 12:03:07 -0400

    Initial Commit

---
+++ Bomb App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bomb App
+//
+//  Created by Patrick Morton on 9/12/15.
+//  Copyright © 2015 Patrick Morton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
